2cb6da263280cf09a25d9ed0f3c2dc179cd1d570
Gregory Vincent Jr <gregvjrr@gmail.com>
2023-12-24 13:14:41 -0500

    Finished the creation of a minimal creation. Shows a message on QEMU

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@
 #![no_std]
 //disable std entry point - libc
 #![no_main]
-
 use core::panic::PanicInfo;
+
+
+// TO RUN WITH QEMU - qemu-system-x86_64 -drive format=raw,file=target/x86_64-buildData/debug/
 
 /**
  * new entry point - no runtime is calling main anymore
@@ -20,17 +22,18 @@ pub extern "C" fn _start() -> ! {
     //mutable pointer to the beginning of the VGA buffer
     let vga_buffer = 0xb8000 as *mut u8;
     //for each byte of data in our message
-    for (i, &character_byteData) in TEST_MESSAGE.iter().enumerate() {
+    for (i, &character_bytedata) in TEST_MESSAGE.iter().enumerate() {
         //code in this block can break safety guarantees
         unsafe {
             //write this character into the vga buffer at the position of the buffer
-            *vga_buffer.offset(i as isize * 2) = character_byteData;
+            *vga_buffer.offset(i as isize * 2) = character_bytedata;
             // increment the pointer for the next character we're writing to the buffer
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
     loop{}
 }
+
 
 
 /// Panic handler for undefined behavior happens
